fbc3c2a1619a82f6e015b307e369aa96d090ac52
iamDyeus <dyeusyt@gmail.com>
2024-03-23 00:46:14 +0530

    add comments

--- src/mediaCollector/YoutubeCollector.py
+++ src/mediaCollector/YoutubeCollector.py
@@ -2,11 +2,34 @@ from utils.YoutubeScrapper import Scraper
 from utils.YoutubeDownloader import MP3Downloader
 
 class YoutubeCollector:
+    """
+    Collects MP3 files from YouTube using a combination of scraping and downloading.
+
+    Attributes:
+    - scraper (Scraper): An instance of the Scraper class for YouTube URL validation and extraction.
+    - downloader (MP3Downloader): An instance of the MP3Downloader class for downloading MP3 files.
+
+    Methods:
+    - collect(input): Collects MP3 files based on the input provided, which can be a YouTube URL, a playlist URL, or a search query.
+    """
+
     def __init__(self):
+        """
+        Initializes the YoutubeCollector with instances of Scraper and MP3Downloader.
+        """
         self.scraper = Scraper()
         self.downloader = MP3Downloader()
 
     def collect(self, input):
+        """
+        Collects MP3 files based on the input provided.
+
+        Parameters:
+        - input (str): The input can be a YouTube URL, a playlist URL, or a search query.
+
+        Returns:
+        - tuple: A tuple containing a boolean indicating the success of the operation and either a single filename (if input is a URL or search query) or a list of filenames (if input is a playlist URL).
+        """
         if self.scraper.is_youtube_url(input):
             url = input
             success, filename = self.downloader.download_mp3(url)
@@ -28,4 +51,3 @@ class YoutubeCollector:
                 return success, filename
             else:
                 return False, None
-
